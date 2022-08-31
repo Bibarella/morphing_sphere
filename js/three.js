e173bc555646a4fc1a4c296b1546633c361a635e
bibarella <glasner.bianca@yahoo.it>
2022-08-31 19:17:25 +0200

    Import Three, Orbitcontrols and Simplex Noise

---
+++ js/three.js
@@ -0,0 +1,3 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.133.1";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls.js";
+import openSimplexNoise from 'https://cdn.skypack.dev/open-simplex-noise';
